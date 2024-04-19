84a78ceff928c75a05a8ca0ef20c282f39f9d2bc
Christiaan83 <c.zielman@gmail.com>
2024-04-19 15:25:13 +0200

    make navigation more responsive

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,12 +1,17 @@
 import './Navigation.css';
 import {NavLink} from "react-router-dom";
 import { User } from "@phosphor-icons/react";
+import { scrollerHelper } from '../../helpers/scrollerHelper.jsx'
 
 
 function Navigation() {
+    const scrolled = scrollerHelper();
+
     return (
-        <nav>
-            <div className="nav-container">
+        // eslint-disable-next-line no-constant-condition
+        <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+
+            <div className= 'nav-container'>
                 <h4><NavLink to="/">MTB Clinics-Ede</NavLink></h4>
 
                 <ul>
