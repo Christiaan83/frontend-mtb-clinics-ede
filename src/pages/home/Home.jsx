cc65da37a46c553f9088f11d6bfd3bb42b8f0660
Christiaan83 <c.zielman@gmail.com>
2024-04-24 10:51:48 +0200

    add footer to component and added shadows

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -95,29 +95,6 @@ function Home() {
                     </ul>
                 </section>
             </main>
-            <footer className='footer'>
-                <section className="footer-section">
-                    <div>
-                        <h3>Contact gegevens</h3>
-                        <a href="https://maps.google.nl/maps?daddr=Akulaan%202,%206717%20XN%20in%20Ede" target="_blank">
-                            <p> De Fietser, Akulaan 2</p>
-                            <p>6717 XN, Ede</p>
-                        </a>
-                        <p><a href="tel:+31318583207">0318 583 207</a></p>
-                        <p><a href="mailto:info@mtbclinics-ede.nl">info@mtbclinics-ede.nl</a></p>
-                    </div>
-                    <div>
-                        <h3><Link to="/">MTB Clinics-ede</Link></h3>
-                    </div>
-
-                    <div>
-                        <Link to="/contact">
-                            <button>Contact</button>
-                        </Link>
-
-                    </div>
-                </section>
-            </footer>
         </>
     );
 }
