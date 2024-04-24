31fdd1081e1b0dcb4da878ea65ab708d321d6605
Christiaan83 <c.zielman@gmail.com>
2024-04-24 15:19:39 +0200

    add mtb-routes path

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Route, Routes} from "react-router-dom";
 import Navigation from "./components/navigation/Navigation.jsx";
 import Home from "./pages/home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import MtbRoutes from "./pages/mtb-routes/MtbRoutes.jsx";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Navigation/>
         <Routes>
             <Route path="/" element={<Home/>} />
+            <Route path="/mtb-routes" element={<MtbRoutes/>} />
         </Routes>
         <Footer/>
     </>
