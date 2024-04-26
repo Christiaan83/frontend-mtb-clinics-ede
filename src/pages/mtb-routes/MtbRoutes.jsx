7a26dd6ddfa679d9894a7ff7d23464f9dd716c9f
Christiaan83 <c.zielman@gmail.com>
2024-04-26 10:35:15 +0200

    added logic and search options

--- src/pages/mtb-routes/MtbRoutes.jsx
+++ src/pages/mtb-routes/MtbRoutes.jsx
@@ -3,8 +3,87 @@ import mtbRoute from "../../assets/mtbRoute.jpg";
 import mtbSymbol from "../../assets/mtb-teken-groen.jpg"
 import Header from "../../components/header/Header.jsx";
 import './MtbRoutes.css'
+import {useEffect, useState} from "react";
+import axios from 'axios';
 
 function MtbRoutes() {
+    const [allRoutes, setAllRoutes] = useState([]);
+    const [filteredRoutes, setFilteredRoutes] = useState([]);
+    const [place, setPlace] = useState('');
+    const [province, setProvince] = useState('');
+    const [difficulty, setDifficulty] = useState('');
+    const [routeType, setRouteType] = useState('');
+    const [error, toggleError] = useState(false);
+
+    useEffect(() => {
+        const fetchAllRoutes = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/routes/search');
+                setAllRoutes(response.data);
+                setFilteredRoutes(response.data);
+            } catch (err) {
+                console.error(err);
+                toggleError(true);
+            }
+        };
+        fetchAllRoutes();
+    }, []);
+
+    const handleSearch = async () => {
+        toggleError(false);
+
+        try {
+            let queryString = 'http://localhost:8080/routes/search?';
+            const params = { place, province, difficulty, routeType };
+            queryString += Object.entries(params)
+                .filter(([key, value]) => value)
+                .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+                .join('&');
+
+            const response = await axios.get(queryString);
+            setFilteredRoutes(response.data);
+        } catch (err) {
+            console.error(err);
+            toggleError(true);
+        }
+    };
+
+    const handlePlaceChange = (event) => {
+        setPlace(event.target.value);
+    };
+
+    const handleProvinceChange = (event) => {
+        setProvince(event.target.value);
+    };
+
+    const handleDifficultyChange = (event) => {
+        setDifficulty(event.target.value);
+    };
+
+    const handleTypeChange = (event) => {
+        setRouteType(event.target.value);
+    };
+
+
+    function changeDifficultyName(difficulty) {
+        switch (difficulty) {
+            case 'EASY':
+                return 'Makkelijk';
+            case 'MODERATE':
+                return 'Gemiddeld';
+            case 'DIFFICULT':
+                return 'Moeilijk';
+        }
+    }
+
+    function changeTypeName(routeType) {
+        switch (routeType) {
+            case 'ADULT':
+                return 'Volwassene';
+            case 'CHILD':
+                return 'Kinderroute';
+        }
+    }
 
     return (
         <>
@@ -19,7 +98,7 @@ function MtbRoutes() {
                     <div className="inner-content-container ">
                         <h2>Zoek de route die bij je past!</h2>
                         <div className="search-info-container">
-                            <img className= "route-img" src={mtbSymbol} alt="MTB-symbool"/>
+                            <img className="route-img" src={mtbSymbol} alt="MTB-symbool"/>
                             <p>Kom crossen over singletracks door bossen en heidevelden, tackle een uitdagende MTB-route
                                 of geniet van één van de mooiste MTB-routes van Nederland! Ben je op zoek naar een
                                 MTB-route voor beginners of ben je juist toe aan een uitdagende track? Kies hier de
@@ -27,11 +106,68 @@ function MtbRoutes() {
                                 het beste bij je past.</p>
                         </div>
                     </div>
-
                 </section>
+                <div>
+                    <div>
+                        <label htmlFor="place">Plaats:</label>
+                            <input type="text"
+                                   id="place"
+                                   value={place}
+                                   onChange={handlePlaceChange}
+                            />
+                    </div>
+                    <div>
+                        <label htmlFor="province">Provincie:</label>
+                        <select id="province"  value={province} onChange={handleProvinceChange}>
+                            <option value="">Kies provincie</option>
+                            <option value="Gelderland">Gelderland</option>
+                            <option value="Utrecht">Utrecht</option>
+                            <option value="Overijssel">Overijssel</option>
+                            <option value="Flevoland">Flevoland</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="difficulty">Niveau:</label>
+                        <select id="difficulty" value={difficulty} onChange={handleDifficultyChange}>
+                            <option value="">Kies Niveau</option>
+                            <option value="EASY">Makkelijk</option>
+                            <option value="MODERATE">Gemiddeld</option>
+                            <option value="DIFFICULT">Moeilijk</option>
+
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="routeType">Type:</label>
+                        <select id="routeType" value={routeType} onChange={handleTypeChange}>
+                            <option value="">Kies Type</option>
+                            <option value="ADULT">Volwassene</option>
+                            <option value="CHILD">Kind</option>
+                        </select>
+                    </div>
+                    <button onClick={handleSearch}>Zoeken</button>
+                    {error && <p>Kan route niet vinden.</p>}
+                    {filteredRoutes.length > 0 ?(
+                    <ul>
+                        {filteredRoutes.map((route)=>(
+                        <li
+                            key={route.id}>
+                                <h3>{route.name}</h3>
+                                <p>Locatie: {route.place}</p>
+                                <p>Provincie: {route.province}</p>
+                                <p>Type: {changeTypeName(route.routeType)}</p>
+                                <p>Niveau: {changeDifficultyName(route.difficulty)}</p>
+                                <p>Afstand: {route.distance}km</p>
+                                <p>Beginpunt: {route.startingPoint}</p>
+                            </li>
+                        ))}
+                    </ul>
+                        ):(
+                            <p>Route niet gevonden, probeer het opnieuw!</p>
+                        )}
+                </div>
             </main>
         </>
-    )
+    );
 }
 
 export default MtbRoutes;
