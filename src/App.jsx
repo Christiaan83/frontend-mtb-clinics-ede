b5287757f47a07cf1358926618a6b866a471822c
Christiaan83 <c.zielman@gmail.com>
2024-04-29 10:45:42 +0200

    added RouteDetails

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navigation from "./components/navigation/Navigation.jsx";
 import Home from "./pages/home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import MtbRoutes from "./pages/mtb-routes/MtbRoutes.jsx";
+import RouteDetails from "./pages/routeDetails/RouteDetails.jsx";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/mtb-routes" element={<MtbRoutes/>} />
+            <Route path="/mtb-routes/:id" element={<RouteDetails/>} />
         </Routes>
         <Footer/>
     </>
