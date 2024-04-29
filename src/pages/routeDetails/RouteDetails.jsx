95eca11b4287aad186b0069b225dfdc5343aa729
Christiaan83 <c.zielman@gmail.com>
2024-04-29 17:14:09 +0200

    add picture to header

--- src/pages/routeDetails/RouteDetails.jsx
+++ src/pages/routeDetails/RouteDetails.jsx
@@ -2,45 +2,49 @@ import axios from "axios";
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import Header from "../../components/header/Header.jsx";
-import mtbRoute from "../../assets/mtbRoute.jpg";
+import routeMtb from '../../assets/header-mtb-route.jpeg';
+import DifficultyName from "../../helpers/changeDifficultyName.jsx";
+import TypeName from "../../helpers/changeTypeName.jsx";
 
 function RouteDetails() {
 
-    const[route, setRoute] = useState(null);
+    const [route, setRoute] = useState(null);
     const {id} = useParams();
     const [image, setImage] = useState(null);
     const [error, toggleError] = useState(false);
 
 
-useEffect(() => {
-    toggleError(true);
+    useEffect(() => {
+        toggleError(true);
 
-    async function getRouteDetails() {
-        try {
-            const response = await axios.get(`http://localhost:8080/routes/${id}`);
-            console.log(response.data);
-            setRoute(response.data);
-        } catch (err) {
-            console.error(err);
-            toggleError(true);
+        async function getRouteDetails() {
+            try {
+                const response = await axios.get(`http://localhost:8080/routes/${id}`);
+                console.log(response.data);
+                setRoute(response.data);
+            } catch (err) {
+                console.error(err);
+                toggleError(true);
+            }
         }
-    }
-    getRouteDetails();
-}, [id]);
+
+        getRouteDetails();
+    }, [id]);
 
     useEffect(() => {
-       async function fetchRoutePicture(){
+        async function fetchRoutePicture() {
             try {
                 const response = await axios.get(`http://localhost:8080/routes/${id}/picture`, {
                     responseType: 'blob',
                 });
                 const imageUrl = URL.createObjectURL(response.data);
                 setImage(imageUrl);
-            }catch (err){
-            console.error("Kan afbeelding niet ophalen", err);
+            } catch (err) {
+                console.error("Kan afbeelding niet ophalen", err);
             }
-       }
-       fetchRoutePicture();
+        }
+
+        fetchRoutePicture();
         return () => {
             if (image) {
                 URL.revokeObjectURL(image);
@@ -49,32 +53,37 @@ useEffect(() => {
     }, [id]);
 
 
+    return (
 
-    return(
-    <main>
-        <Header img={mtbRoute} img_title="bike-wheel" title="MTB-Routes"/>
-        <div>
+        <main>
 
             {route ? (
-                <div>
+                <section>
+                    <Header img={routeMtb} img_title={route.name} title={route.name}/>
                     <h2>{route.name}</h2>
+                    <div>
                     <p>Provincie: {route.province}</p>
                     <p>Afstand: {route.distance}km</p>
-                    <p>Type: {route.routeType}</p>
+                    <p>Type: {TypeName(route.routeType)}</p>
+                    <p>Niveau: {DifficultyName(route.difficulty)}</p>
+                    </div>
+
                     {image && <img src={image} alt='Route image'/>}
+                    <div>
                     <h4>Startlocatie</h4>
                     <p>Je kunt de route starten bij: </p>
                     <li>{route.startingPoint}</li>
                     <h3>Route Informatie</h3>
                     <p>{route.routeInformation}</p>
-                </div>
+                    </div>
+                </section>
             ) : (
                 <p>{error} Route aan het laden....</p>
             )}
 
-        </div>
-    </main>
-);
+
+        </main>
+    );
 }
 
 export default RouteDetails;
