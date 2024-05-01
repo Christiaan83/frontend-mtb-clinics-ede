f1fd2f219472a3ff7cd6c97792e5e52b8220c645
Christiaan83 <c.zielman@gmail.com>
2024-05-01 16:24:57 +0200

    added retalPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import {Route, Routes} from "react-router-dom";
 import Navigation from "./components/navigation/Navigation.jsx";
 import Home from "./pages/home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import MtbRoutes from "./pages/mtb-routes/MtbRoutes.jsx";
+import MtbRoutes from "./pages/mtbRoutes/MtbRoutes.jsx";
 import RouteDetails from "./pages/routeDetails/RouteDetails.jsx";
+import RentalPage from "./pages/mtbRentalPage/RentalPage.jsx"
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/mtb-routes" element={<MtbRoutes/>} />
             <Route path="/mtb-routes/:id" element={<RouteDetails/>} />
+            <Route path="/mtb-verhuur" element={<RentalPage/>}/>
         </Routes>
         <Footer/>
     </>
