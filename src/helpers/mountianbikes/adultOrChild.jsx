06cb483773a5509f503a625b7f8f3cdcd6b3c31b
Christiaan83 <c.zielman@gmail.com>
2024-05-01 16:27:18 +0200

    added helpers for rentalPage

---
+++ src/helpers/mountianbikes/adultOrChild.jsx
@@ -0,0 +1,10 @@
+function adultOrChild(forAdult) {
+    switch (forAdult) {
+        case true:
+            return 'Volwassen mountainbike';
+        case false:
+            return 'Kinder mountainbike';
+    }
+}
+
+export default adultOrChild;
