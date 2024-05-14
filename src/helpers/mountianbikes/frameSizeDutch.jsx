c8383f367be9b1f969033acc034ac3336103d13a
Christiaan83 <c.zielman@gmail.com>
2024-05-14 10:46:46 +0200

    added name converter

---
+++ src/helpers/mountianbikes/frameSizeDutch.jsx
@@ -0,0 +1,13 @@
+function frameSizeDutch(frameSize) {
+    switch (frameSize) {
+        case 'small':
+            return 'Klein';
+        case 'medium':
+            return 'Medium';
+        case 'large':
+            return 'Groot';
+        case 'extra-large':
+            return 'Extra groot';
+    }
+}
+export default frameSizeDutch;
