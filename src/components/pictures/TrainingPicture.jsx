02dc0ffd4cbfa4b0adec06d809bc9219aba8a6dd
Christiaan83 <c.zielman@gmail.com>
2024-05-27 15:08:23 +0200

    Added Training picture component

---
+++ src/components/pictures/TrainingPicture.jsx
@@ -0,0 +1,42 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+// eslint-disable-next-line react/prop-types
+const TrainingPicture = ({ training, trainingId}) => {
+    const [imageUrl, setImageUrl] = useState(null);
+
+    useEffect(() => {
+        const fetchTrainingPicture = async () => {
+            try {
+
+                const response = await axios.get(`http://localhost:8080/trainings/${trainingId}/picture`, {
+                    responseType: 'blob',
+                });
+                const newImageUrl = URL.createObjectURL(response.data);
+                setImageUrl(newImageUrl);
+            } catch (err) {
+                console.error("Kan afbeelding niet ophalen");
+            }
+        };
+
+        fetchTrainingPicture();
+
+        return () => {
+            if (imageUrl) {
+                URL.revokeObjectURL(imageUrl);
+            }
+        };
+    }, [training, trainingId]);
+
+    return (
+        <div>
+            {imageUrl ? (
+                <img src={imageUrl} alt={`Training ${trainingId}`} />
+            ) : (
+                <p>Loading image...</p>
+            )}
+        </div>
+    );
+};
+
+export default TrainingPicture;
