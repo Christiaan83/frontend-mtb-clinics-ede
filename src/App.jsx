29e7f61a41add7f4b91d5d5b3b66c033ab754c0f
Christiaan83 <c.zielman@gmail.com>
2024-05-27 15:10:48 +0200

    Added new page

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Home from "./pages/home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import MtbRoutes from "./pages/mtbRoutes/MtbRoutes.jsx";
 import RouteDetails from "./pages/routeDetails/RouteDetails.jsx";
-import RentalPage from "./pages/mtbRentalPage/RentalPage.jsx"
-import BookRentalPage from "./pages/bookRentalPage/BookRentalPage.jsx";
+import RentalPage from "./pages/mtbRental/RentalPage.jsx"
+import BookRentalPage from "./pages/bookRental/BookRentalPage.jsx";
+import MtbClinics from "./pages/mtbClinics/MtbClinics.jsx";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Navigation/>
         <Routes>
             <Route path="/" element={<Home/>} />
+            <Route path="/mtb-clinics" element={<MtbClinics/>} />
             <Route path="/mtb-routes" element={<MtbRoutes/>} />
             <Route path="/mtb-routes/:id" element={<RouteDetails/>} />
             <Route path="/mtb-verhuur" element={<RentalPage/>}/>
